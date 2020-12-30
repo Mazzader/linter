adaf8109a3a1330fcc3eaf8cde7d6b795305459d
n.pushkar <napushkar@gmail.com>
2020-12-30 16:54:08 +0500

    linter fix

--- linter/linter.py
+++ linter/linter.py
@@ -2,61 +2,74 @@ import re
 
 res_len_str = {}
 res = {}
-reg_exp_brace_in_func = re.compile('^function [.]+\([.]+\)$')
+reg_exp_brace_in_func = re.compile('^function')
 
 
-def space_in_func_check(upload_files, *args):
+def space_in_func_check(*args):
     pass
 
 
-def space_in_loop_check(upload_files, *args):
-    for i in range(len(upload_files)):
-        for string in upload_files[i]:
-            if string.find('for'):
-                if string[string.find('for'):string.find('for') + 4] != 'for ':
-                    res_len_str[upload_files[i]] = string
+def space_in_loop_check(*args):
+    #TODO дописать эту хуйню дома!
+    with open('test.js', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.find('for') != -1:
+                if line[line.find('for'):line.find('for') + 4] != 'for ':
+                    try:
+                        res_len_str[line].append(' Ошибка в пробелах цикла ')
+                    except KeyError:
+                        res_len_str[line] = []
+                        res_len_str[line].append(' Ошибка в пробелах цикла')
 
 
-def space_in_args_check(upload_files, *args):
-    for i in range(len(upload_files)):
-        for string in upload_files[i]:
-            if re.search(reg_exp_brace_in_func, string) is not None:
-                buffer = string.split("(")[1].split(")")[0]
+def space_in_args_check(*args):
+    with open('test.js', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if re.search(reg_exp_brace_in_func, line) is not None:
+                buffer = line.split("(")[1].split(")")[0]
                 for i in range(1, len(buffer)):
                     if buffer[i - 1] == ',' and buffer[i] != ' ':
-                        res_len_str[upload_files[i]] = string
+                        try:
+                            res_len_str[line].append(' Ошибка в пробелах ')
+                        except KeyError:
+                            res_len_str[line] = []
+                            res_len_str[line].append(' Ошибка в пробелах ')
 
 
 def sintyx_in_func_code(upload_files, *args):
     pass
 
 
-def brace_check(upload_files, *args):
-    for i in range(len(upload_files)):
-        for string in upload_files[i]:
-            if re.search(reg_exp_brace_in_func, string) is not None \
-                    and string.split(")") != " {\n":
-                res_len_str[upload_files[i]] = string
+def brace_check(*args):
+    with open('test.js', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if re.search(reg_exp_brace_in_func, line) is not None \
+                    and line.split(")") != " {\n":
+                try:
+                    res_len_str[line].append(' Ошибка в скобочках) ')
+                except KeyError:
+                    res_len_str[line] = []
+                    res_len_str[line].append(' Ошибка в скобочках) ')
 
 
-def len_of_string_check(upload_files, *args):
-    for i in range(len(upload_files)):
-        for string in upload_files[i]:
-            if len(string) > 120:
-                res_len_str[upload_files[i]] = string
+
+def len_of_string_check(*args):
+    with open('test.js', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if len(line) > 120:
+                try:
+                    res_len_str[line].append(' Ошибка в длине строки, длина строки больше 120 символов!')
+                except KeyError:
+                    res_len_str[line] = []
+                    res_len_str[line].append(' Ошибка в длине строки, длина строки больше 120 символов!')
 
 
-def handle_uploaded_file(f, uu_id):
-    print(f)
-    with open('{}.txt'.format(uu_id), 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
-
-
-def linter_handler(f):
-    len_of_string_check(f)
-    brace_check(f)
-    space_in_args_check(f)
-    space_in_loop_check(f)
-    space_in_func_check(f)
-    return res_len_str
+len_of_string_check()
+brace_check()
+space_in_args_check()
+space_in_loop_check()
+print(res_len_str)
